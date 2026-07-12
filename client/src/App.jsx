9b558512d8c225314b006e73ed4a329549baec5a
sangtosha <116915060+Vincentsang254@users.noreply.github.com>
2026-07-12 04:01:12 +0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,59 +1,138 @@
+/* eslint-disable react/prop-types */
 import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
-import AdminLayout from "./components/admin-view/layout";
-import AdminDashboard from "./pages/admin-view/dashboard/dashboard";
-import NotFound from "./pages/not-found";
+import AdminLayout from "./components/admin-view/common/layout";
+import UserLayout from "./components/user-view/common/layout";
 import CheckAuth from "./components/common/check-auth";
-import UnauthPage from "./pages/unauth-page";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+// Auth Pages
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
-import { loadUser, setUserLoaded } from "./features/slices/authSlice";
 import ForgotPassword from "./pages/auth/forgot-password";
-import AdminUsers from "./pages/admin-view/management/users/admin-users";
-
-import AdminPayments from "./pages/admin-view/management/payments/payments";
-
+import VerifyAccount from "./pages/auth/verify-account";
+import CheckEmail from "./pages/auth/check-email";
 import ResetPassword from "./pages/auth/password-reset";
-import ProductUpload from "./pages/product-upload";
-import PropertyUpload from "./pages/property-upload";
 
-const App = () => {
+// Admin Pages
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminUsers from "./pages/admin-view/admin-users";
+
+// User Pages
+import Home from "./pages/user-view/common/Home";
+import Profile from "./pages/user-view/account/Profile";
+
+// Common Pages
+import UnauthPage from "./pages/unauth-page";
+import NotFound from "./pages/not-found/Notfound";
+import AdminPaymentsHistory from "./pages/admin-view/admin-view-payments";
+import { loadUser, refreshToken } from "./features/slices/authSlice";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+const App = ({ isAuthenticated, user }) => {
+
   const dispatch = useDispatch();
-  const userLoaded = useSelector((state) => state.auth.userLoaded);
+  const { token } = useSelector((state) => state.auth);
 
-  console.log("User Loaded:", userLoaded);
+  console.log("token: ", token)
 
   useEffect(() => {
-    const initializeAuth = async () => {
+    if (token) {
+      dispatch(refreshToken());
+    }
+  }, [token, dispatch]);
+ 
 
-      const token = localStorage.getItem("accessToken");
-      if (token) {
-        dispatch(loadUser());
-      } else {
-        dispatch(setUserLoaded());
-      }
-    };
-
-    initializeAuth();
+  // Load user on app initialization
+  useEffect(() => {
+    dispatch(loadUser());
   }, [dispatch]);
-
-  if (!userLoaded) {
-    return (
-      <div className="flex flex-col overflow-hidden bg-white min-h-screen">
-        <Skeleton className="w-full h-screen bg-gray-200" />
-      </div>
-    );
-  }
-
   return (
-    <div className="flex flex-col overflow-hidden bg-white min-h-screen">
-      <h1>App</h1>
-   <p>Welcome to the GBox App!</p>
+    <div className="flex flex-col min-h-screen overflow-hidden bg-white">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              {/* Home or landing page can go here */}
+            </CheckAuth>
+          }
+        />
+
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+          <Route path="verify-otp" element={<VerifyAccount />} />
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="check-email" element={<CheckEmail />} />
+        </Route>
+
+        <Route path="/auth/reset-password/:token" element={<ResetPassword />} />
+
+        {/* Admin Dashboard Routes */}
+        <Route
+          path="/admin"
+          element={
+            <CheckAuth
+              isAuthenticated={isAuthenticated}
+              user={user}
+              requireAuth
+              requireAdmin
+            >
+              <AdminLayout />
+            </CheckAuth>
+          }
+        >
+          <Route index element={<AdminDashboard />} />
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="users" element={<AdminUsers />} />
+          
+          <Route path="payments" element={<AdminPaymentsHistory />} />
+        </Route>
+
+        {/* User Dashboard Routes */}
+        <Route
+          path="/user"
+          element={
+            <CheckAuth
+              isAuthenticated={isAuthenticated}
+              user={user}
+              requireAuth
+            >
+              <UserLayout />
+            </CheckAuth>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Home />} />
+          <Route path="profile" element={<Profile />} />
+          
+        </Route>
+
+        {/* Common pages */}
+        <Route path="/unauth-page" element={<UnauthPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 };
